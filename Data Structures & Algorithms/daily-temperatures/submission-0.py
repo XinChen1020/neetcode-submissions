ce9fc8d8a4d79f74fe835ae0555aaa54cf3cfacf
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-05-29 18:24:22 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        mono_stack = deque()
+        result = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            while mono_stack and temperatures[i] > mono_stack[-1][0]:
+                temp, idx = mono_stack.pop()
+                result[idx] = i - idx
+            mono_stack.append((temperatures[i], i))
+        return result
