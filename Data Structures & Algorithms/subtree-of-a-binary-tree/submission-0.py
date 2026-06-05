34c5a0c4f9454015f4a26588244e1e5a3bddf1b8
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-05 02:32:29 -0700

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # BFS
+        stack_p = deque([p])
+        stack_q = deque([q])
+
+        while stack_p:
+            for _ in range(len(stack_p)):
+                node_p = stack_p.popleft()
+                node_q = stack_q.popleft()
+
+                if not node_p and not node_q:
+                    continue
+                if not node_p or not node_q or node_p.val != node_q.val:
+                    return False
+                stack_p.append(node_p.left)
+                stack_p.append(node_p.right)
+
+                stack_q.append(node_q.left)
+                stack_q.append(node_q.right)
+        return True
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # DFS
+        stack = deque([root])
+
+        while stack:
+            node = stack.pop()
+            if not node:
+                continue
+            if node.val == subRoot.val:
+                if self.isSameTree(node, subRoot):
+                    return True
+            stack.append(node.left)
+            stack.append(node.right)
+        return False
