b0f7d928becef95763e6d8e8329bf4ed38f43c9f
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-12 12:29:14 -0700

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+       
+        result = []
+        freq_map = Counter(candidates)
+        candidates = sorted(freq_map.keys())
+
+        stack = deque([[0, 0, [], freq_map]])
+
+        while stack:
+            curr_idx, curr_sum, last_result, freq_left = stack.pop()
+
+            if curr_sum == target:
+                result.append(last_result)
+
+            for i in range(curr_idx, len(candidates)):
+                for j in range(1, freq_left[candidates[i]] + 1):
+                    if curr_sum + j * candidates[i] <= target:
+                        freq_left[candidates[i]] -= j
+                        stack.append([i + 1, curr_sum + j *candidates[i], last_result + j * [candidates[i]], freq_left.copy()])
+                    else:
+                        break
+        return result
+
+
