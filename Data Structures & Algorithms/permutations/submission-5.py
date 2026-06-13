b7812b906141bcdc651f05e1971029ce3c8fc0c7
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-12 17:48:42 -0700

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # each decision branch is to swap with element at start with an
+        # later element
+        result = []
+
+        def dfs(start, nums):
+
+            nonlocal result
+            if start == len(nums):
+                result.append(nums.copy())
+                return
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                dfs(start + 1, nums)
+                nums[start], nums[i] = nums[i], nums[start]
+
+        dfs(0, nums)
+        return result
