b4401cee5f7e4bc23bff415b26a6a0312f9368f1
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-20 16:38:51 -0700

    Add: palindrome-partitioning - submission-6

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # Backtracking with DP
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+
+        # Need to go backward since dp[l][r] depends on 
+        # dp[l+1][r-1]/the lower left corner
+        for l in range(n - 1, -1, -1):
+            for r in range(l, n):
+                # is pailndrome is inner str is pailndrome and the letter
+                # on the two ends are the same
+                if s[l] == s[r] and (r - l <= 2 or dp[l + 1][r - 1]):
+                    dp[l][r] = True
+        
+        result = []
+        stack = deque([[0, []]])
+
+        while stack:
+            # l = start of the next partition
+            l, partitions = stack.pop()
+
+            
+            if l == len(s):
+                result.append(partitions)
+
+            # Find the next potential partitions and add
+            # to the list
+            # r = end of the next partition/substring
+            for r in range(l, len(s)):
+                # Prune out partition if it's not forming a pailndrome
+                if dp[l][r]:
+                    next_partitions = partitions.copy()
+                    next_partitions.append(s[l:r + 1])
+                    stack.append([r + 1, next_partitions])
+
+        return result
