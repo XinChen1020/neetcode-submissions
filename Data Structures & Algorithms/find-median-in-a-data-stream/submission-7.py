2dc679973f366cf516dd523293ca36b8f18ffde9
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-20 01:16:38 -0700

    Add: find-median-in-a-data-stream - submission-7

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-7.py
@@ -0,0 +1,45 @@
+class MedianFinder:
+
+    def __init__(self):
+        # max_heap -> lower half
+        # min_heap -> higher half
+        # Let top of max_heap to be the median when there's 
+        # odd numbers and average of max_heap[0] and min_heap[0]
+        # when there's even numbers
+        self.max_heap = []
+        self.min_heap = []
+
+        # use for rebalance
+        self.max_heap_size = 0
+        self.min_heap_size = 0
+
+    def addNum(self, num: int) -> None:
+        # Fill in lower heap for initial element 
+        if not self.max_heap:
+            heapq.heappush(self.max_heap, -num)
+            self.max_heap_size += 1
+            return
+    
+        if num <= -self.max_heap[0]:
+            heapq.heappush(self.max_heap, -num)
+            self.max_heap_size += 1
+        else:
+            heapq.heappush(self.min_heap, num)
+            self.min_heap_size += 1
+        
+        # rebalance
+        if self.max_heap_size - self.min_heap_size > 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+            self.max_heap_size -= 1
+            self.min_heap_size += 1
+        elif self.min_heap_size - self.max_heap_size >= 1:
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+            self.min_heap_size -= 1
+            self.max_heap_size += 1
+
+
+    def findMedian(self) -> float:
+
+        return (-self.max_heap[0] + self.min_heap[0]) / 2 if self.max_heap_size == self.min_heap_size else -self.max_heap[0]
+        
+        
