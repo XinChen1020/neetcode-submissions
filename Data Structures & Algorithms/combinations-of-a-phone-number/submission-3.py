56938c709a2c0a3bcef1a322a3c1c81f77f93d25
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-20 12:48:53 -0700

    Add: combinations-of-a-phone-number - submission-3

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        results = []
+        stack = deque([[0, ""]])
+        digits_mapping = {"2": "abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz"}
+
+        while stack:
+            idx, path = stack.pop()
+
+            if idx == len(digits):
+                results.append(path)
+                continue
+
+            for letter in digits_mapping[digits[idx]]:
+                next_path = path
+                next_path += letter
+                stack.append([idx + 1, next_path])
+        
+        return results
