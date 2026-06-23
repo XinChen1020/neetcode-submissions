e4c463c9720a08298e630345f92dd84235e5304e
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-23 01:38:53 -0700

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        # start bfs from each rotten fruit.
+        # minimum number of min is the max distance between
+        # rotten fruit and fresh fruit when all rotten fruit start
+        # bfs at the same time
+        result = 0
+        queue = deque([])
+        search_dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        
+
+        def bfs():
+            nonlocal result
+            while queue:
+                i, j, minute = queue.popleft()
+                result = max(result, minute)
+                for d in search_dir:
+                    new_i = i + d[0]
+                    new_j = j + d[1]
+
+                    if 0 > new_i or new_i >= len(grid) or \
+                    0 > new_j or new_j >= len(grid[0]) or \
+                    grid[new_i][new_j] != 1:
+                        continue
+
+                    grid[new_i][new_j] = 2
+                    queue.append((new_i, new_j, minute + 1))
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    queue.append((i, j, 0))
+        
+        bfs()
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    return -1
+
+        return result
