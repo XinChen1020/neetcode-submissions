b0a490ad1c986f3ff0adf13dc1a2f6be862deb8c
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-06-27 16:45:18 -0700

    Add: redundant-connection - submission-8

---
+++ Data Structures & Algorithms/redundant-connection/submission-8.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # khan algo
+        # Since there n-1 edges, there's n node
+        degree = [0] * (len(edges) + 1)
+        adj = [[] for _ in range(len(edges) + 1)]
+        
+        # Craft adj list
+        for u, v in edges:
+            adj[u].append(v)
+            degree[u] += 1
+            adj[v].append(u)
+            degree[v] += 1
+        
+        queue = deque([])
+        for i in range(1, len(edges) + 1):
+            if degree[i] == 1:
+                queue.append(i)
+        
+        while queue:
+            node = queue.popleft()
+
+            degree[node] -= 1
+
+            for nei in adj[node]:
+                degree[nei] -= 1
+                if degree[nei] == 1:
+                    queue.append(nei)
+        
+        for u, v in reversed(edges):
+            if degree[u] == 2 and degree[v]:
+                return [u, v]
+        return []
+
+
+            
+
+
