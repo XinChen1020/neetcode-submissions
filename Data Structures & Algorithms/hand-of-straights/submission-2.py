1b5da09296db1c0202c17e549e95865f4c037fd8
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-07-26 01:26:37 -0700

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # hand must be multiple of groupSize
+        if len(hand) % groupSize != 0:
+            return False
+        
+        # How to form the group
+        # Always start with the smallest and go up until groupSize
+        # then repeat from the smallest again
+        c = Counter(hand)
+        
+        # Start with all groups at the same time
+        for k in sorted(c.keys()):
+            if c[k] > 0:
+                num_group = c[k]
+
+                for i in range(groupSize):
+                    if c[k + i] < num_group:
+                       return False
+                    c[k + i] -= num_group
+
+        return True
