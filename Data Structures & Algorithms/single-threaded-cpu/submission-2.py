3de55c7b289b7efdce018b1155286531873ac947
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-08-31 12:42:42 -0700

    Add: single-threaded-cpu - submission-2

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        heap = []
+        result = []
+
+        for i in range(len(tasks)):
+            tasks[i].append(i)
+
+        tasks.sort()
+
+        curr_time = 0
+        i = 0
+
+        while heap or i < len(tasks):
+
+            # If CPU is idle, jump to the next available task
+            if not heap and curr_time < tasks[i][0]:
+                curr_time = tasks[i][0]
+
+            # Add every task that has become available
+            while i < len(tasks) and tasks[i][0] <= curr_time:
+                enqueue, processing, idx = tasks[i]
+                heapq.heappush(heap, (processing, idx))
+                i += 1
+
+            processing, idx = heapq.heappop(heap)
+            result.append(idx)
+            curr_time += processing
+
+        return result
