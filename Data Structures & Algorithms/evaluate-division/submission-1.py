64471563bc33cf2d6b4d9414d6fb9808b5e4e2d6
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-09-07 23:30:59 -0700

    Add: evaluate-division - submission-1

---
+++ Data Structures & Algorithms/evaluate-division/submission-1.py
@@ -0,0 +1,42 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        
+        weighted_adj = defaultdict(list)
+
+        # Construct graph
+        for i, e in enumerate(equations):
+            x, y = e
+            weighted_adj[x].append((y, values[i]))
+            weighted_adj[y].append((x, 1 / values[i]))
+        
+        # DFS
+        visited = set()
+        def dfs(u, target):
+
+            if u == target:
+                return 1
+            
+            if u in visited:
+                return -1
+
+            visited.add(u)
+
+            for v, weight in weighted_adj[u]:
+                result = dfs(v, target)
+                
+                if result != -1:
+                    visited.remove(u)
+                    return result * weight
+
+            visited.remove(u) 
+            return -1
+        
+        result = []
+        for u, v in queries:
+            if u not in weighted_adj or v not in weighted_adj:
+                result.append(-1)
+                continue
+            result.append(dfs(u, v))
+        return result
+
+
