31e76670611643ebc310f48880dbcd17de195f93
Xin Chen <68122993+XinChen1020@users.noreply.github.com>
2026-09-20 12:42:58 -0700

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # Prime algo
+        # Use priority heap
+        # Assume all point separate, start with a random point
+        # always pick the minimal edge connect to Vs that
+        # are currently in the MST to an outside Vs, which avoids cycle
+
+        # distance from unvisited to visited node, idx
+        heap = [[0, 0]]
+        visited = [False] * len(points)
+        cost = 0
+        edges = 0
+
+        distance = lambda u, v: abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+
+        while edges < len(points):
+            dist, node = heapq.heappop(heap)
+
+            if visited[node]:
+                continue
+
+            cost += dist
+            visited[node] = True
+            edges += 1
+
+            for i in range(len(points)):
+                if not visited[i]:
+                    heapq.heappush(heap, [distance(node, i), i])
+        return cost
+        
+
